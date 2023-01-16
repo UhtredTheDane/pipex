cb07e185f3cdb4cd47fd2e3aea16f91336c113f4
Alexis GENGEMBRE <agengemb@e1r8p6.clusters.42paris.fr>
2023-01-16 23:43:48 +0100

    premiere base

--- srcs/main.c
+++ srcs/main.c
@@ -6,50 +6,59 @@
 /*   By: agengemb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 22:01:42 by agengemb          #+#    #+#             */
-/*   Updated: 2023/01/15 22:58:46 by agengemb         ###   ########.fr       */
+/*   Updated: 2023/01/16 21:57:27 by agengemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#define PROCESSUS_NB 2
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-void	receiver(char **envp, char *out_file, char **argv, int *pipe_fd)
+#define PROCESSUS_NB 2
+char	**ft_split(char const *s, char c);
+
+void	receiver(char **envp, char **argv, int *pipe_fd)
 {
-	int	*ft_stdout;
+	int	fd_stdout;
 	char **cmd;
 
 	close(pipe_fd[1]);
-	cmd = ft_split(argv[4], ' ');
-	fd_stdout = open(argv[3], O_WRONLY);
+	cmd = ft_split(argv[3], ' ');
+	if (!cmd)
+		printf("gros bug\n");
+	fd_stdout = open(argv[4], O_WRONLY | O_CREAT);
 	if (fd_stdout == -1)
 	{
 		close(pipe_fd[0]);
-		exit (1);
+		exit(1);
 	}
 	if (dup2(pipe_fd[0], 0) == -1)
 	{
 		close(pipe_fd[0]);
-		close(ft_stdout);
+		close(fd_stdout);
 		exit(2);
 	}
 	if (dup2(fd_stdout, 1) == -1)
 	{
 		close(pipe_fd[0]);
-		close(ft_stdout);
+		close(fd_stdout);
 		exit(2);
 	}
 	if (execve(cmd[0], cmd, envp) == -1)
 	{
 		close(pipe_fd[0]);
-		close(ft_stdout);
+		close(fd_stdout);
 		exit(3);
 	}
-	
 }
 
-void	sender(char **envp, char *in_file, char **argv, int *pipe_fd)
+void	sender(char **envp, char **argv, int *pipe_fd)
 {
-	int	*ft_stdin;
+	int	fd_stdin;
 	char **cmd;
 
 	close(pipe_fd[0]);
@@ -60,27 +69,33 @@ void	sender(char **envp, char *in_file, char **argv, int *pipe_fd)
 		close(pipe_fd[1]);
 		exit (1);
 	}
+	if (dup2(fd_stdin, 0) == -1)
+	{
+		close(pipe_fd[1]);
+		close(fd_stdin);
+		exit(2);
+	}
 	if (dup2(pipe_fd[1], 1) == -1)
 	{
 		close(pipe_fd[1]);
 		close(fd_stdin);
 		exit(2);
 	}
-	if (cmd(argv[0], cmd, envp) == -1)
+	if (execve(cmd[0], cmd, envp) == -1)
 	{
 		close(pipe_fd[1]);
 		close(fd_stdin);
-		exit();
+		exit(3);
 	}
 	
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	size_t	i;
 	int pipe_fd[2];
 	pid_t	pid;
 	int status;
+	size_t i;
 
 	if (argc != 5)
 	{
@@ -91,19 +106,25 @@ int	main(int argc, char **argv, char **envp)
 	if (pipe(pipe_fd))
 		return (0);
 
-
+	i = 0;
 	while (i < PROCESSUS_NB)
 	{
 		pid = fork();
-		if (pid == 0)
+		if (pid < 0)
+		{
+			perror("Erreur fork");
+			return (1);
+		}
+		else if (pid == 0)
 		{
 			if (i == 0)
-				sender(envp, argv[1], argv[2], pipe);
+				sender(envp, argv, pipe_fd);
 			else
-				receiver(envp, argv[3], argv[4], pipe);
+				receiver(envp, argv, pipe_fd);
 		}
+		++i;
 	}
-	while (waitpid(pid, &status, 0) == -1)
-		;
+	i = 0;
+	waitpid(-1, &status, 0);
 	return (0);
 }
